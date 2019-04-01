e887f06499594d170a0b616e337d4eeaa87e2b63
Peter McGowan <p.mcgowan1@gmail.com>
2019-04-01 22:00:14 +0100

    Simplified summary statistics with dataframe.describe() Add Python File
    INSERT

--- iris_analysis.py
+++ iris_analysis.py
@@ -14,7 +14,9 @@ df = pd.DataFrame(f) # Create a dataframe from the csv data
 
 #print(df)
 
-print(f"Sepal Length: Max={df['sepal_length'].max()} Min={df['sepal_length'].min()} Mean={round(df['sepal_length'].mean(),2)} Median={df['sepal_length'].median()} StDev={round(df['sepal_length'].std(),2)}")
-print(f"Sepal Width: Max={df['sepal_width'].max()} Min={df['sepal_width'].min()} Mean={round(df['sepal_width'].mean(),2)} Median={df['sepal_width'].median()} StDev={round(df['sepal_width'].std(),2)}")
-print(f"Petal Length: Max={df['petal_length'].max()} Min={df['petal_length'].min()} Mean={round(df['petal_length'].mean(),2)} Median={df['petal_length'].median()} StDev={round(df['petal_length'].std(),2)}")
-print(f"Petal Width: Max={df['petal_width'].max()} Min={df['petal_width'].min()} Mean={round(df['petal_width'].mean(),2)} Median={df['petal_width'].median()} StDev={round(df['petal_width'].std(),2)}")
+#print(f"Sepal Length: Max={df['sepal_length'].max()} Min={df['sepal_length'].min()} Mean={round(df['sepal_length'].mean(),2)} Median={df['sepal_length'].median()} StDev={round(df['sepal_length'].std(),2)}")
+#print(f"Sepal Width: Max={df['sepal_width'].max()} Min={df['sepal_width'].min()} Mean={round(df['sepal_width'].mean(),2)} Median={df['sepal_width'].median()} StDev={round(df['sepal_width'].std(),2)}")
+#print(f"Petal Length: Max={df['petal_length'].max()} Min={df['petal_length'].min()} Mean={round(df['petal_length'].mean(),2)} Median={df['petal_length'].median()} StDev={round(df['petal_length'].std(),2)}")
+#print(f"Petal Width: Max={df['petal_width'].max()} Min={df['petal_width'].min()} Mean={round(df['petal_width'].mean(),2)} Median={df['petal_width'].median()} StDev={round(df['petal_width'].std(),2)}")
+
+print(round(df.describe(percentiles=[]),3)) # Summary statistics, rounded to 3 decimal places

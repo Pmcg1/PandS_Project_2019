fae1c6edd6088a778434208cab9b0da82a142fff
Peter McGowan <p.mcgowan1@gmail.com>
2019-04-08 23:00:38 +0100

    added scatter matrix

--- iris_analysis.py
+++ iris_analysis.py
@@ -6,6 +6,7 @@
 # Import numpy and pandas for analysis
 import numpy as np
 import pandas as pd
+from pandas.plotting import scatter_matrix
 import matplotlib.pyplot as plt
 
 
@@ -13,18 +14,17 @@ f = pd.read_csv('iris.csv') # Import csv file using pandas
 
 df = pd.DataFrame(f) # Create a dataframe from the csv data
 
-#print(df)
+# print(df)
 
-#print(f"Sepal Length: Max={df['sepal_length'].max()} Min={df['sepal_length'].min()} Mean={round(df['sepal_length'].mean(),2)} Median={df['sepal_length'].median()} StDev={round(df['sepal_length'].std(),2)}")
-#print(f"Sepal Width: Max={df['sepal_width'].max()} Min={df['sepal_width'].min()} Mean={round(df['sepal_width'].mean(),2)} Median={df['sepal_width'].median()} StDev={round(df['sepal_width'].std(),2)}")
-#print(f"Petal Length: Max={df['petal_length'].max()} Min={df['petal_length'].min()} Mean={round(df['petal_length'].mean(),2)} Median={df['petal_length'].median()} StDev={round(df['petal_length'].std(),2)}")
-#print(f"Petal Width: Max={df['petal_width'].max()} Min={df['petal_width'].min()} Mean={round(df['petal_width'].mean(),2)} Median={df['petal_width'].median()} StDev={round(df['petal_width'].std(),2)}")
 
 print(round(df.describe(percentiles=[]),3)) # Summary statistics, rounded to 3 decimal places
 
 
-df.plot(kind='box', subplots=True, layout=(2,2), grid = True) # Display box plots
-df.plot(kind='hist', subplots=True, layout=(2,2), sharex=False, grid = True) # Display histograms
+df.plot(kind='box', subplots=True, layout=(2,2), grid = True, sharey= False) # Display box plots
+
+df.plot(kind='hist', subplots=True, layout=(2,2), sharex=False, sharey = True, grid = True) # Display histograms
+
+scatter_matrix(df) # added basic scatter matrix but need to format
 
 plt.show()
 
